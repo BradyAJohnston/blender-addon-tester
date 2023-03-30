05f97dfa9ee64b8fa87c110c52c57af9b225fa6b
Brady <brady.johnston@me.com>
2023-03-30 12:01:01 +0800

    fix coverage.xml

--- blender_addon_tester/blender_load_pytest.py
+++ blender_addon_tester/blender_load_pytest.py
@@ -6,18 +6,12 @@ import os
 from pathlib import Path
 import sys
 import pytest
-print("place 1")
-# try:
-# except Exception as e:
-    # print(e)
-    # #sys.exit(1)
 
 # Make sure to have BLENDER_ADDON_TO_TEST set as an environment variable first
 ADDON = os.environ.get("BLENDER_ADDON_TO_TEST", False)
 if not ADDON:
     print("No addon to test was found in the 'BLENDER_ADDON_TO_TEST' environment variable. Exiting.")
     #sys.exit(1)
-print("place 2")
 
 # Set any value to the BLENDER_ADDON_COVERAGE_REPORTING environment variable to enable it
 COVERAGE_REPORTING = os.environ.get("BLENDER_ADDON_COVERAGE_REPORTING", False)
@@ -27,7 +21,6 @@ TESTS_PATH = os.environ.get("BLENDER_ADDON_TESTS_PATH", default_tests_dir.as_pos
 # Add explict pytest commands, just in case fine control is required
 PYTEST_ARGS = os.environ.get("BLENDER_PYTEST_ARGS", "")
 
-print("place 3")
 try:
     sys.path.append(os.environ["LOCAL_PYTHONPATH"])
     from addon_helper import zip_addon, change_addon_dir, install_addon, cleanup
@@ -35,7 +28,6 @@ except Exception as e:
     print(e)
     #sys.exit(1)
 
-print("place 4")
 # This is needed for windows, doesn't seem to affect linux
 addon_helper = os.environ.get("ADDON_TEST_HELPER", None)
 if None == addon_helper:
@@ -43,7 +35,6 @@ if None == addon_helper:
 elif os.path.isdir(addon_helper):
     sys.path.append(addon_helper)
 
-print("place 5")
 class SetupPlugin:
     def __init__(self, addon):
         self.addon = addon
@@ -61,17 +52,14 @@ class SetupPlugin:
         cleanup(self.addon, self.bpy_module, self.addon_dir)
         print("*** test run reporting finished")
 
-
-print("place 6")
 try:
     pytest_main_args = [TESTS_PATH, "-v", "-x"]
-    if COVERAGE_REPORTING is not False:
-        print("place 7")
-        pytest_main_args += ["--cov", "--cov-report", "term", "--cov-report", "xml"]
+    if True:
+        pytest_main_args += ["--cov", "--cov-report", "term", 
+                            "--cov-report", "xml:coverage.xml"]
         if not "" == PYTEST_ARGS:
             pytest_main_args += [PYTEST_ARGS]
     exit_val = pytest.main(pytest_main_args, plugins=[SetupPlugin(ADDON)])
-    print("place 8")
 except Exception as e:
     print(e)
     exit_val = 1
